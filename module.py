8d61c78b1b20a726d9ddae1cd49110af2d7139c0
Nikolai <nikolaiperlt@gmail.com>
2019-04-05 14:55:54 +0200

    hej

--- module.py
+++ module.py
@@ -7,40 +7,49 @@ import zipfile
 import requests
 import os
 
-# TODO: Download data
 
-data_url = 'http://www.kaggle.com/c/digit-recognizer/download/train.csv'
-os.environ["KAGGLE_USERNAME"] = "perlten"
-os.environ["KAGGLE_KEY"] = "29374bfab081c879e9b2ac2a896e748e"
-os.system("kaggle datasets download -d PromptCloudHQ/imdb-data")
+def download_data():
+    os.environ["KAGGLE_USERNAME"] = "perlten"
+    os.environ["KAGGLE_KEY"] = "2184df94c35b67eaaef1ca9377e0b785"
+    os.system("kaggle datasets download -d PromptCloudHQ/imdb-data")
 
-zip_ref = zipfile.ZipFile("imdb-data.zip", 'r')
-zip_ref.extractall()
-zip_ref.close()
+    zip_ref = zipfile.ZipFile("imdb-data.zip", 'r')
+    zip_ref.extractall()
+    zip_ref.close()
 
-data = pd.read_csv("IMDB-Movie-Data.csv")
 
-data = data[pd.notnull(data["Metascore"])]
-data = data[pd.notnull(data["Revenue (Millions)"])]
+if __name__ == "__main__":
+    os.environ["KAGGLE_USERNAME"] = "perlten"
+    os.environ["KAGGLE_KEY"] = "29374bfab081c879e9b2ac2a896e748e"
+    os.system("kaggle datasets download -d PromptCloudHQ/imdb-data")
 
-xs = data["Metascore"]
-ys = data["Revenue (Millions)"]
+    zip_ref = zipfile.ZipFile("imdb-data.zip", 'r')
+    zip_ref.extractall()
+    zip_ref.close()
 
-xs_reshape = np.array(xs).reshape(-1, 1)
+    data = pd.read_csv("IMDB-Movie-Data.csv")
 
-x_train, x_test, y_train, y_test = train_test_split(xs_reshape, ys)
+    data = data[pd.notnull(data["Metascore"])]
+    data = data[pd.notnull(data["Revenue (Millions)"])]
 
-model = sklearn.linear_model.LinearRegression()
+    xs = data["Metascore"]
+    ys = data["Revenue (Millions)"]
 
-model.fit(x_train, y_train)
+    xs_reshape = np.array(xs).reshape(-1, 1)
 
-print(model.score(x_test, y_test))
-y_predict = model.predict(x_test)
+    x_train, x_test, y_train, y_test = train_test_split(xs_reshape, ys)
 
-plt.scatter(x_test, y_test,  color='black')
-plt.plot(x_test, y_predict, color='blue', linewidth=3)
+    model = sklearn.linear_model.LinearRegression()
 
-plt.xticks(())
-plt.yticks(())
+    model.fit(x_train, y_train)
 
-plt.show()
+    print(model.score(x_test, y_test))
+    y_predict = model.predict(x_test)
+
+    plt.scatter(x_test, y_test,  color='black')
+    plt.plot(x_test, y_predict, color='blue', linewidth=3)
+
+    plt.xticks(())
+    plt.yticks(())
+
+    plt.show()
